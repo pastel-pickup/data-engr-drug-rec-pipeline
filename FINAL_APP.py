421f56ff6a71212771805c8dcb5641f7230e1cc6
Mai Tran <67651332+pastel-pickup@users.noreply.github.com>
2022-08-07 18:30:28 -0400

    Add files via upload

---
+++ FINAL_APP.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine, MetaData, text
+import streamlit as st
+
+engine = create_engine('sqlite:///data/drugdb.db', echo=True)
+
+meta = MetaData()
+
+tq = text("SELECT * FROM top_drugs")
+conn = engine.connect()
+result = conn.execute(tq).fetchall()
+st.write(result)
+
+
+
+
+
+
+
+
+/*condition = data.condition.unique()
+
+st.title("PeopleRX: A Drug Recommendation System For People By The People")
+
+st.header("This system recommends top 3 most effective medications for a chosen medical condition")
+
+st.subheader("Enter a medical condition:")
+
+st.selectbox("Condition", condition, index=1)
+
+def drug_rec(condition):
+    engine = create_engine("sqlite:///C:/Users/New User/Desktop/drugdb.db")
+    drug_results = pd.read_sql("SELECT COUNT(*)OVER(PARTITION BY drugName) AS reviews, drugName, condition, rating, rank FROM drug_top WHERE condition= '+condition+' ORDER BY rating DESC;", con=engine, index_col='index')
+    drug_results = drug_results.drop_duplicates()
+    print(drug_results[:3])
+
+if st.button("Recommend Me:"):
+    drug_rec(condition)
+
+st.text("Disclaimer: \nThis system and its recommendations are not official medical advice. \nPlease use at your own risk.") */
