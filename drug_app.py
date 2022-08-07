25f427fc232ad58f10263a7281bfe31a354c3bd6
Mai Tran <67651332+pastel-pickup@users.noreply.github.com>
2022-08-07 14:26:29 -0400

    Update drug_app.py

--- drug_app.py
+++ drug_app.py
@@ -5,13 +5,14 @@ from sqlalchemy import create_engine
 import pyodbc
 import pymysql
 
-#creating connection
+
 
 engine = sqlalchemy.create_engine(
     "mssql+pyodbc://DESKTOP-H9GNET5/New User:M4iSQLServer@DESKTOP-H9GNET5/mydb?driver=ODBC+Driver+17+for+SQL+Server", 
     )
 
+conn = engine.connect()
 
-data = pd.read_sql('SELECT * mytable;', engine)
+data = pd.read_sql('SELECT * mytable;', conn)
 
 st.write(data)
