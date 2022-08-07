3eb8fba3352c41bc106643888169c43e5af95337
Mai Tran <67651332+pastel-pickup@users.noreply.github.com>
2022-08-07 13:53:36 -0400

    Add files via upload

---
+++ drug_app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+from sqlalchemy import create_engine
+import pyodbc
+import pymysql
+
+#creating connection
+
+@st.cache(allow_output_mutation=True)
+def get_connection():
+    return create_engine("mssql+pyodbc://username:M4iSQLServer@DESKTOP-H9GNET5/mydb?driver=ODBC+Driver+17+for+SQL+Server", 
+    fast_executemany = True
+    )
+
+%%time
+
+data = pd.read_sql('SELECT * FROM dbo.mytable', engine)
+
+st.write(data)
