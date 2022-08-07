9cc17134a76a44c028a609acd25f530fa1595c65
Mai Tran <67651332+pastel-pickup@users.noreply.github.com>
2022-08-07 11:45:53 -0400

    Update drug_rec_app2.py

--- drug_rec_app2.py
+++ drug_rec_app2.py
@@ -6,7 +6,7 @@ import pyodbc
 @st.experimental_singleton
 def init_connection():
     return pyodbc.connect(
-        "DRIVER={ODBC Driver 13 for SQL Server};SERVER="
+        "DRIVER={ODBC Driver 15 for SQL Server};SERVER="
         + st.secrets["server"]
         + ";DATABASE="
         + st.secrets["database"]
